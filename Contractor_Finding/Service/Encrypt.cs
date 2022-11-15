2fc7df642173ccbb11da52ea6f39f8d0cf6c90ce
tarunnag <tarunn@moback.com>
2022-11-15 20:43:29 +0530

    servicePart

---
+++ Contractor_Finding/Service/Encrypt.cs
@@ -0,0 +1,25 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Encrypt : IEncrypt
+    {
+        public string Decrypt_Password(string encryptpassword)
+        {
+            string pswstr = string.Empty;
+            System.Text.UTF8Encoding encode_psw = new System.Text.UTF8Encoding();
+            System.Text.Decoder Decode = encode_psw.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(encryptpassword);
+            int charCount = Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            pswstr = new String(decoded_char);
+            return pswstr;
+        }
+    }
+}
