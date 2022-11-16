f05bdac93cb6c3e2f2f941ad8c41625f9639281f
tarunnag <113530491+tarunnag@users.noreply.github.com>
2022-11-16 21:26:08 +0530

    Revert "servicePart"

--- Contractor_Finding/Service/Interfaces/IUserService.cs
+++ Contractor_Finding/Service/Interfaces/IUserService.cs
@@ -1,5 +1,4 @@
-﻿using Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +8,5 @@ namespace Service.Interfaces
 {
     internal interface IUserService
     {
-        List<UserDetail> GetUserDetails();
-        bool Create(UserDetail userDetail);
-        bool DeleteUser(UserDetail userDetail);
-
-
     }
 }
