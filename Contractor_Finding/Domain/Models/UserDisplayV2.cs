d36e158ef45183de8c12a6b92b2ac01cf9fc6569
KhadeejaShirinVP <khadeejasvp@moback.com>
2022-12-09 18:18:48 +0530

    AutoMapper_Versioning-Completed

---
+++ Contractor_Finding/Domain/Models/UserDisplayV2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class UserDisplayV2
+    {
+        public int UserId { get; set; }
+
+        public string? Usertype { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string? LastName { get; set; }
+
+        public string EmailId { get; set; } = null!;
+
+        public long PhoneNumber { get; set; }
+    }
+}
